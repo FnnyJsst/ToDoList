e62283c18c7c87acd88c9d1c455bc4d7435dc2f5
FnnyJsst <fanny.jousset@gmail.com>
2023-12-14 15:11:33 +0100

    HTML architecture

---
+++ script.js
@@ -0,0 +1,8 @@
+const questions = [
+["Quel animal est associé à la maison Gryffondor à Poudlard?", ['Serpent, Hibou, Lion'], 'Lion'], 
+["Quel sortilège est utilisé pour désarmer un adversaire dans le monde de Harry Potter?", ["Expelliarmus", "Wingardium Leviosa", 
+"Expecto Patronum"], "Expelliarmus"],
+["Qui est le professeur de potions à Poudlard pendant la plupart de la série Harry Potter?", ["Professeur McGonagall", "Professeur Flitwick", 
+'Professeur Rogue'], "Professeur Rogue"],
+];
+

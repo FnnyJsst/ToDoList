520f38756ab0ecb11d437d684d2a66b55986f623
FnnyJsst <fanny.jousset@gmail.com>
2023-12-15 14:53:18 +0100

    Update on score is working

--- script.js
+++ script.js
@@ -17,46 +17,59 @@ const quizzData = [
 ];
 
 const quizContainer = document.getElementById('quiz-container');
+let score = 0;
 
+
+//Fonction pour afficher les questions et réponses
+//Fonction pour afficher les questions et réponses
 function displayQuestions() {
+    const pUpdatedScore = document.querySelector('.score'); // Utiliser querySelector pour obtenir un élément unique
+    pUpdatedScore.innerHTML = `Score : ${score}`;
+
     quizzData.forEach((currentQuizzData) => {
         const questionContainer = document.createElement('div');
         questionContainer.classList.add('question-container');
-        
+
         const questionElement = document.createElement('p');
         questionElement.textContent = currentQuizzData.question;
+
         questionContainer.appendChild(questionElement);
 
-        let answerButtons = [];  
-        let isAnswered = false;  
+        let answerButtons = [];
+        let isAnswered = false;
 
+        // On place chaque option de réponse dans un bouton
         currentQuizzData.options.forEach((option) => {
             const answerButton = document.createElement('button');
             answerButton.textContent = option;
             questionContainer.appendChild(answerButton);
 
+            // Appels des fonctions de vérification de réponse et de sélection d'un seul bouton
             answerButton.addEventListener('click', () => {
-                if (!isAnswered) {  
+                if (!isAnswered) {
                     checkAnswer(option, currentQuizzData.correctAnswer, answerButton);
                     deleteButtons(currentQuizzData.correctAnswer, answerButtons);
-                    isAnswered = true;  
+                    isAnswered = true;
+                    // Mettre à jour le score après chaque réponse
+                    pUpdatedScore.innerHTML = `Score = ${score}`;
                 }
             });
-
-            answerButtons.push(answerButton);  
+            answerButtons.push(answerButton);
         });
-
         quizContainer.appendChild(questionContainer);
     });
 
+    // Fonction pour vérifier si la réponse est vraie ou fausse
     function checkAnswer(selectedAnswer, correctAnswer, buttonElement) {
         if (selectedAnswer === correctAnswer) {
             buttonElement.style.backgroundColor = '#18391E';
+            score++;
         } else {
             buttonElement.style.backgroundColor = '#7F0000';
         }
     }
 
+    // Fonction qui permet de ne sélectionner qu'un seul bouton
     function deleteButtons(correctAnswer, buttons) {
         buttons.forEach((button) => {
             if (button.textContent !== correctAnswer) {
@@ -65,6 +78,7 @@ function displayQuestions() {
         });
     }
 }
+
 
 displayQuestions();
 
